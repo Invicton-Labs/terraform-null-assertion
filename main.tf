f556ea12ddd4e5ea494d10623f8acd9b530219be
Kyle Kotowick <kyle@kotowick.ca>
2021-07-06 18:00:47 -0400

    Add Terraform version

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+terraform {
+  required_version = ">= 0.13.0"
+}
+
 // Terraform has no built-in customizable error system, so instead we try to run a non-existant program with the error message in the name.
 // It's not pretty, but it works.
 data "external" "assertion" {
