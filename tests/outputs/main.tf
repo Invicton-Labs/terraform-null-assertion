5d2c67336415d29015cea80f30fb2fe7ed4f8afd
KyleKotowick <kotowick@invictonlabs.com>
2023-03-26 12:42:27 -0400

    Testing

---
+++ tests/outputs/main.tf
@@ -0,0 +1,25 @@
+variable "key1" {
+  type = number
+}
+variable "key2" {
+  type = string
+}
+variable "key3" {
+  type = list(string)
+}
+variable "key4" {
+  type = map(string)
+}
+
+output "key1" {
+  value = var.key1
+}
+output "key2" {
+  value = var.key1
+}
+output "key3" {
+  value = var.key1
+}
+output "key4" {
+  value = var.key1
+}
