00a8319394a314c3355154dfb3e9ee2b8cf1ee7c
Kyle Kotowick <kotowick@invictonlabs.com>
2024-07-07 12:05:50 -0400

    Refactor to use new variable validation capabilities

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,14 @@
+output "error_message" {
+  description = "The value of the `error_message` input variable."
+  value       = var.error_message
+}
+
+output "condition" {
+  description = "The value of the `condition` input variable."
+  value       = var.condition
+}
+
 output "checked" {
   description = "Whether the condition has been checked (used for assertion dependencies)."
-  value       = data.cloudinit_config.check.rendered == "" ? true : true
+  value       = var.condition == true ? true : true
 }
